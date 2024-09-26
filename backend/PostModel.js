832731de8f6c5dc45b33291fa1d5f9116b80be0b
Mohammed Rayaan Pasha <134199206+mdrayaanpasha@users.noreply.github.com>
2024-09-26 15:08:52 +0530

    Update PostModel.js

--- backend/PostModel.js
+++ backend/PostModel.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema(
-    {
-        College:String,
-        UserId:String,
-        UserName:String,
-        An: Boolean,
-        Re: Boolean,
-        Desc:String,
-        Course:String
-    }
-);
+const PostSchema = new mongoose.Schema({
+    College: String,
+    UserId: String,
+    UserName: String,
+    An: Boolean,
+    Re: Boolean,
+    Desc: String,
+    Course: String,
+    Img: String
+});
 
 const PostModel = mongoose.model('PostModel', PostSchema);
 
